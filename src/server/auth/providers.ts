f0f62db2f8db1315903a063747ef247eb51f7bdb
Ronaldo Kereh <kreh.whoami@gmail.com>
2025-05-02 21:19:04 +0800

    initial commit

---
+++ src/server/auth/providers.ts
@@ -0,0 +1,23 @@
+import type { Provider } from "next-auth/providers";
+import NextAuth from "next-auth";
+import Discord from "next-auth/providers/discord";
+
+const providers: Provider[] = [Discord];
+
+export const providerMap = providers
+  .map((provider) => {
+    if (typeof provider === "function") {
+      const providerData = provider();
+      return { id: providerData.id, name: providerData.name };
+    } else {
+      return { id: provider.id, name: provider.name };
+    }
+  })
+  .filter((provider) => provider.id !== "credentials");
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers,
+  pages: {
+    signIn: "/login",
+  },
+});
