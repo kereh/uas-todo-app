1ba8c3836baeae2c4128ec8636c02aabcf8ba111
Ronaldo Kereh <kreh.whoami@gmail.com>
2025-05-05 14:32:32 +0800

    fix(ui): update task card styles and icons

--- src/components/tasks/tasks-card.tsx
+++ src/components/tasks/tasks-card.tsx
@@ -1,10 +1,10 @@
 import { memo } from "react";
 import type { Task } from "@/types";
+import { cn } from "@/lib/utils";
 import * as Sortable from "@/components/ui/sortable";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { Edit2, Check, Cog } from "lucide-react";
+import { Check, SlidersHorizontal } from "lucide-react";
 
 const TasksCardComponent = ({ id, task, isComplete }: Task) => {
   return (
@@ -12,23 +12,23 @@ const TasksCardComponent = ({ id, task, isComplete }: Task) => {
       <Card className={cn("p-0 transition-all duration-300 hover:shadow-lg")}>
         <CardContent className="p-4">
           <div className="flex items-center justify-between">
-            <div>
+            <div className="flex items-center">
               <p
                 className={cn(
-                  "truncate text-base font-medium",
+                  "truncate text-sm font-medium",
                   isComplete && "text-muted-foreground line-through",
                 )}
               >
                 {task}
               </p>
-              {isComplete && (
-                <span className="mt-1 inline-flex items-center rounded-full px-2 text-xs font-medium text-green-600">
-                  <Check className="mr-1" />
-                  Completed
-                </span>
-              )}
             </div>
-            <Cog />
+            {isComplete && (
+              <span className="mt-1 inline-flex items-center rounded-full px-2 text-sm font-medium text-green-600">
+                <Check />
+                Completed
+              </span>
+            )}
+            <SlidersHorizontal className="h-4 w-4 cursor-pointer" />
           </div>
         </CardContent>
       </Card>
