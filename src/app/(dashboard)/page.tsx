60ad78ee06c2fa5c0e06298276474c6d2208e658
Ronaldo Kereh <kreh.whoami@gmail.com>
2025-05-05 20:54:05 +0800

    chore(ui): replace sortable with listbox component

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,51 +1,36 @@
 "use client";
 
 import type { Task } from "@/types";
-import * as Sortable from "@/components/ui/sortable";
 import { TasksAdd } from "@/components/tasks/tasks-add";
 import { TasksCard } from "@/components/tasks/tasks-card";
+import {
+  Listbox,
+  ListboxItem,
+  ListboxItemIndicator,
+} from "@/components/ui/listbox";
 import { api } from "@/trpc/react";
-import { useState, useEffect } from "react";
 
 export default function Page() {
-  const [tasks, setTasks] = useState<Task[]>([]);
   const { data, isLoading } = api.tasks.getAllTask.useQuery();
-
-  useEffect(() => {
-    if (data) {
-      setTasks(data);
-    }
-  }, [data]);
 
   return (
     <div className="flex flex-col gap-5">
       <div className="w-auto">
         <TasksAdd />
       </div>
-      <Sortable.Root
-        value={tasks}
-        onValueChange={setTasks}
-        getItemValue={(item) => item.id}
-        orientation="mixed"
-      >
-        {isLoading ? (
-          "loading..."
-        ) : (
-          <Sortable.Content className="grid auto-rows-fr grid-cols-3 gap-3 rounded-md">
-            {tasks?.map((task) => (
-              <TasksCard
-                key={task.id}
-                id={task.id}
-                task={task.task}
-                isComplete={task.isComplete}
-              />
-            ))}
-          </Sortable.Content>
-        )}
-        <Sortable.Overlay>
-          <div className="bg-primary/10 size-full rounded-md" />
-        </Sortable.Overlay>
-      </Sortable.Root>
+      <Listbox orientation="mixed" className="grid w-full gap-2 sm:grid-cols-3">
+        {data?.map((task) => (
+          <ListboxItem key={task.id} value={task.task!} className="items-start">
+            <div className="flex flex-col gap-px">
+              <div className="font-medium">{task.task}</div>
+              <div className="text-muted-foreground line-clamp-2 text-sm">
+                {task.isComplete ? "Completed" : "Complete"}
+              </div>
+            </div>
+            <ListboxItemIndicator />
+          </ListboxItem>
+        ))}
+      </Listbox>
     </div>
   );
 }
